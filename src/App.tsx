ded530899dcf01cd4675a18384806df2d912b818
lhx <huanxlu@163.com>
2022-12-12 13:49:37 +0800

    根据name展示不同icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import React from "react";
 import Icon from "./Icon";
 
 function App() {
   return (
     <div>
       <Icon name="wechat" />
+      <Icon name="alipay" />
+      <Icon name="qq" />
     </div>
   );
 }
--- src/Icon/index.tsx
+++ src/Icon/index.tsx
@@ -1,9 +1,20 @@
+import React from "react";
+import wechat from "../icons/wechat.svg";
+import alipay from "../icons/alipay.svg";
+import qq from "../icons/qq.svg";
+
 interface IconProps {
   name: string;
 }
 
-const Icon: React.FunctionComponent<IconProps> = (props) => {
-  return <span>{props.name}</span>;
+const Icon: React.FunctionComponent<IconProps> = ({ name }) => {
+  return (
+    <>
+      {name === "wechat" && <img src={wechat} alt="wechat" />}
+      {name === "alipay" && <img src={alipay} alt="alipay" />}
+      {name === "qq" && <img src={qq} alt="qq" />}
+    </>
+  );
 };
 
 export default Icon;
